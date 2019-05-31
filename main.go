13e21e1a011b4f0f828e6097f55b3015b3c64991
Victoria Jeffrey <vjeffrey@chef.io>
2019-05-31 16:04:17 -0600

    add cors header

--- main.go
+++ main.go
@@ -31,6 +31,8 @@ func main() {
 }
 
 func nodesHandler(w http.ResponseWriter, r *http.Request) {
+    	w.Header().Set("Access-Control-Allow-Origin", "*")
+    	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	decoder := json.NewDecoder(r.Body)
 	var t types.NodesRequest
 	err := decoder.Decode(&t)
